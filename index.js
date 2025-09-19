3053470a3d586e17b04cea3b096665078182d9d5
BEN <ukiscarlet@gmail.com>
2025-09-19 09:17:30 +0800

    1.增加抽餐點功能 2.抽籤換圖

--- index.js
+++ index.js
@@ -39,11 +39,11 @@ async function handleEvent(event) {
   const groupId = event.source.groupId || event.source.roomId;
   if (!groupId) return; // 個人聊天不處理
 
+  // ======= 抽籤功能 =======
   if (text.includes("hands together")) {
     const fortunes = ["大吉", "中吉", "小吉", "吉", "末吉", "凶", "大凶"];
     const pick = fortunes[Math.floor(Math.random() * fortunes.length)];
 
-    // 中文 URL encode
     const imageUrl = `https://${process.env.RAILWAY_STATIC_URL}/images/${encodeURIComponent(pick)}.png`;
 
     try {
@@ -53,7 +53,50 @@ async function handleEvent(event) {
         previewImageUrl: imageUrl
       });
     } catch (err) {
-      console.error("pushMessage 失敗：", err);
+      console.error("抽籤 pushMessage 失敗：", err);
+    }
+  }
+
+  // ======= 今日吃什麼功能 =======
+  if (text.includes("meat")) {
+    const foods = [
+      "すき焼き",
+      "うな重",
+      "おにぎり",
+      "おでん",
+      "しゃぶしゃぶ",
+      "懐石料理",
+      "焼肉",
+      "弁当",
+      "オムライス",
+      "丼物",
+      "素麺",
+      "ちゃんこ鍋",
+      "うなぎの蒲焼き",
+      "焼きそば",
+      "パスタ",
+      "卵かけご飯",
+      "お茶漬け",
+      "日本のパン",
+	  "ラーメン",
+	  "寿司",         
+      "お好み焼き",   
+      "たこ焼き",     
+      "カレーライス",  
+      "うどん",                  
+      "焼き鳥",       
+      "天ぷら",        
+      "餃子"  
+    ];
+    const pick = foods[Math.floor(Math.random() * foods.length)];
+
+    try {
+      await client.pushMessage(groupId, {
+        type: "text",
+        text: `今日のおすすめは…「${pick}」です！`
+      });
+    } catch (err) {
+      console.error("吃什麼 pushMessage 失敗：", err);
     }
   }
 }
